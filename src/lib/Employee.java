1eb96d3a18b3c41b41ad7b5dc512a3755609c7a4
abdulghofur12 <ghofursans12@gmail.com>
2023-04-13 20:03:12 +0700

    long method
    menghindari variable yang tidak perlu di simpan

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -51,7 +51,7 @@ public class Employee {
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
-	
+	// long method
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
 			monthlySalary = 3000000;
